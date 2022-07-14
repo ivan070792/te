880bf6d59e77204ad9d7c978271ebd9f27992f42
ws01 <ws01@bgpk15.local>
2022-07-14 16:04:59 +0700

    fix report categoty data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Hospital::factory(5)->create();
-        ReportCategory::factory(5)->create();
+        // ReportCategory::factory(5)->create();
+        ReportCategory::insert([
+            [
+                'name' => 'Скорая помощь',
+                'description' => 'Проблемы с вызовом скорой помощи'
+            ],
+            [
+                'name' => 'Електроннная регистрация',
+                'description' => 'Проблемы с электронной регитсрацией'
+            ],
+            [
+                'name' => 'Доступ в больницу',
+                'description' => 'Проблемы с доступом в больницу'
+            ],
+            [
+                'name' => 'Иное',
+                'description' => 'Иные проблемы'
+            ]
+        ]);
         ReportUser::factory(15)->create();
         Report::factory(50)->create();
         User::insert([

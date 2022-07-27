85984927801975e21948ded19dfc019ef53902f2
ws01 <ws01@bgpk15.local>
2022-07-27 17:54:33 +0700

    fixxx

---
+++ app/Libraries/MyClass.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Libraries;
+
+// use App\Models\Report;
+
+class MyClass {
+
+    public function my_my(){
+        // $report->find(1)->get();
+        return true;
+    }
+
+}
---
+++ lang/ru/forms.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'report_status_title' => 'Статус обращения',
+    'report_category_title' => 'Категория обращения',
+    'filter_button' => 'Фильтр',
+    'filter_clear_button' => 'Отчитсить'
+
+];
